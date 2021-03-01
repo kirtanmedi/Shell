6e284cad50767bbdf9c85a129c17c94697f96bbb
kirtanmedi <kirtanmedi@gmail.com>
2021-03-01 02:50:23 -0600

    implemented runBuiltIn

--- shell.c
+++ shell.c
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 
-void printArray(char **array){
+void printArray(char **array) {
     int i = 0;
-    while(array[i] != NULL){
+    while (array[i] != NULL) {
         printf("%d: %s ", i, array[i]);
         i++;
     }
@@ -14,17 +14,17 @@ void printArray(char **array){
 }
 
 //prints the prompt before every command
-void printPrompt(){
+void printPrompt() {
     char dir[1024];
     getcwd(dir, sizeof(dir));
     printf("%s>", dir);
 }
 
 //checks if "exit" or "quit" is entered
-void checkExit(const char *inputLine){
+void checkExit(const char *inputLine) {
     char exitInput[] = "exit";
     char quitInput[] = "quit";
-    if(strcmp(inputLine, exitInput) == 10 || strcmp(inputLine, quitInput) == 10){
+    if (strcmp(inputLine, exitInput) == 10 || strcmp(inputLine, quitInput) == 10) {
         exit(0);
     }
 }
@@ -32,7 +32,7 @@ void checkExit(const char *inputLine){
 #define MAX_INPUT_LENGTH 1024
 
 //reads in the line of commands
-char *readLine(){
+char *readLine() {
     char *inputLine = malloc(sizeof(char) * MAX_INPUT_LENGTH);
     size_t inputLength = 0;
     getline(&inputLine, &inputLength, stdin);
@@ -41,7 +41,7 @@ char *readLine(){
         exit(EXIT_FAILURE);
     }
 
-    checkExit(inputLine);
+    //checkExit(inputLine);
 
     return inputLine;
 }
@@ -49,7 +49,7 @@ char *readLine(){
 #define MAX_NUM_ARGUMENTS 64
 
 //parse the input and figure out which type of execution to run
-char **parseInput(char *inputLine){
+char **parseInput(char *inputLine) {
     char **argList = malloc(sizeof(char *) * MAX_NUM_ARGUMENTS);
     char *argument;
     int i = 0;
@@ -68,47 +68,95 @@ char **parseInput(char *inputLine){
 }
 
 //process input to figure out which type of execution is needed
-int processInput(char **argList){
-    //if pipe command return 2
-    const char pipe[] = "|";
-    if (strcmp(argList[1], pipe) == 0) {
-        return 2;
-    }
-
+int processInput(char **argList) {
     //if built in commands
     const char *builtInCommands[4];
     int i;
-    builtInCommands[0] = "help";
+    builtInCommands[0] = "help\n";
     builtInCommands[1] = "cd";
-    builtInCommands[2] = "exit";
-    builtInCommands[3] = "quit";
+    builtInCommands[2] = "exit\n";
+    builtInCommands[3] = "quit\n";
     for (i = 0; i < 4; i++) {
         if (strcmp(argList[0], builtInCommands[i]) == 0) {
             return 0;
         }
     }
 
-    return 1;
+    //if shell command return 1
+    if(argList[1] == NULL){
+        return 1;
+    }
+
+    //if pipe command return 2
+    const char pipe[] = "|";
+    if (strcmp(argList[1], pipe) == 0) {
+        return 2;
+    }
+}
+
+void helpSh() {
+    printf("this is help\n");
+}
+
+void cdSh() {
+    printf("this is cd\n");
+}
+
+void exitSh() {
+    printf("this is exit\n");
+}
+
+void runBuiltIn(char **argList) {
+    const char *builtInCommands[4];
+    builtInCommands[0] = "help\n";
+    builtInCommands[1] = "cd";
+    builtInCommands[2] = "exit\n";
+    builtInCommands[3] = "quit\n";
+
+    if (strcmp(argList[0], builtInCommands[0]) == 0) {
+        helpSh();
+    } else if (strcmp(argList[0], builtInCommands[1]) == 0) {
+        cdSh();
+    } else if (strcmp(argList[0], builtInCommands[2]) == 0) {
+        exitSh();
+    } else if (strcmp(argList[0], builtInCommands[3]) == 0) {
+        exitSh();
+    }
+}
+
+void runSh(char **argList) {
+    printf("this is shell\n");
+}
+
+void runPipe(char **argList) {
+    printf("this is pipe\n");
 }
 
 //the main looping logic of the shell
-void shell(){
+void shell() {
     char *inputLine;
     char **argList;
     int commandType = 1;
 
-    while(1){
+    while (1) {
         printPrompt();
         inputLine = readLine();
         argList = parseInput(inputLine);
         commandType = processInput(argList);
-        printf("%d", commandType);
+        if (commandType == 0) {           //to run built in commands
+            runBuiltIn(argList);
+        } else if (commandType == 1) {    //to run other shell commands
+            runSh(argList);
+        } else if (commandType == 2) {    //to run piped commands
+            runPipe(argList);
+        }
         free(inputLine);
+        free(argList);
     }
 }
 
 //main method
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     shell();
 
     return 0;
